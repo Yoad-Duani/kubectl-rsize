67e550ecb3f55d55ae7a823325cc20053d3a70f8
Yoad <yoad787@gmail.com>
2023-10-23 00:33:25 +0300

    init rsize

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 Yoad Duani - rsize
+Licensed under the Apache License, Version 2.0 (the "License");
+*/
+package main
+
+import "kubectl-rsize/cmd"
+
+func main() {
+	cmd.Execute()
+}
